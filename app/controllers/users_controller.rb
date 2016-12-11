28b52dd8dcc0db8aa2963cb10ffca20dde414f82
Tony Luisi <t.luisi@gmail.com>
2016-12-11 16:15:20 +1300

    added user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+    def new
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: {
+                message: 'fail'
+            }
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+end
